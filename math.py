b4e6b888adfd2c64779bcb6975ccb0cc71a404e9
Rachel Kenyani <rachelkenyani@gmail.com>
2023-03-31 15:52:26 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,19 @@
+def add(num1,num2):
+    result=num1+num2
+    return result
+
+def subtract(num1,num2):
+    result2=num1-num2
+    return result2
+
+def multiply(num1,num2):
+    result3=num1*num2
+    return result3
+
+def divide(num1,num2):
+    result4=num1/num2
+    return result4
+
+def modulus(num1,num2):
+    result5=num1%num2
+    return result5
